bcab2f9077832381a14fc90814713aee90a63afb
Anthony Lombardo <anthonylombardo@live.com>
2010-11-20 16:56:38 -0500

    reports

--- Clear Choice/Views/CompletedLotsReport.xaml.cs
+++ Clear Choice/Views/CompletedLotsReport.xaml.cs
@@ -25,7 +25,6 @@ namespace Clear_Choice.Views
 
         public CompletedLotsReport()
         {
-            InitializeComponent();
             loadTable();
             this.Name = "CompletedLotReport";
         }
