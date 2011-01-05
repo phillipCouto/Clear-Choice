01ceed8009626b06047d8ba2dab70487ab636f70
Phillip Couto <phillip.couto@stemstudios.com>
2011-01-05 08:51:42 -0500

    Added a new project for the Stemstudios File Service API. This project will define the api for uploading and downloading files from the File Service.
    Signed-off-by: Phillip Couto <phillip.couto@stemstudios.com>

---
+++ Stemstudios File Service/FileService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Stemstudios_File_Service
+{
+    public class FileService
+    {
+
+        public static FileService Instance
+        {
+            get { return instance; }
+        }
+
+        private static FileService instance = new FileService();
+
+        static FileService()
+        { }
+        FileService()
+        { }
+
+        public String UploadFile(String appID, String file)
+        {
+            return "";
+        }
+
+        public String GetDownloadURL(String appID, String fileID)
+        {
+            return "";
+        }
+    }
+}
